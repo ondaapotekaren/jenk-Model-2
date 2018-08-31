e75adcc19396a4ffeffb9f15fdbca1ba6fcce896
ondaapotekaren <viktor_berglund@hotmail.com>
2018-08-31 13:05:37 +0200

    add average calc and gitignorefile

---
+++ calc_avg_and_var.py
@@ -0,0 +1,25 @@
+import sys
+import math
+with open(sys.argv[1]) as f:
+	content = f.readlines() 
+content = [x.strip() for x in content]
+print('\n' + "----Results----" + '\n')
+print("content: " + str(content) + '\n')
+
+mean = 0
+for i in content:
+	mean += float(i)
+
+mean = mean/len(content)
+
+var = 0
+for i in content:
+	var += ((float(i) - mean) ** 2)
+
+var = var/len(content)
+
+std = math.sqrt(var)
+
+print("mean: " + str(mean) + '\n')
+print("variance: " + str(var) + '\n')
+print("standard deviation: " + str(std))
